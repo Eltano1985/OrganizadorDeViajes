5a1b980ae369d06dfb8f360e4a73f0452c050a03
Eltano1985 <e.amoresano85@gmail.com>
2024-11-08 19:22:58 -0300

    documentado information

--- information.js
+++ information.js
@@ -1,14 +1,15 @@
 import Destination from "./Destination.js";
 import ImagesCarousel from "./ImagesCarousel.js";
 
+/**
+ * Event that is executed when the document content has been completely loaded. Retrieves the selected destination from local storage and loads the destination data and images.
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const selectedDestination = localStorage.getItem('destinoSeleccionado');
     
-    // Inicializar el gestor del destino
     const destinationManager = new Destination(selectedDestination);
     destinationManager.loadDestinationData();
 
-    // Inicializar y cargar el carrusel de imágenes
     const carousel = new ImagesCarousel(selectedDestination);
     carousel.loadImages();
 });
