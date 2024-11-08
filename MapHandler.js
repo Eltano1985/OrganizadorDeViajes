0bdf7b15e1382f0c297401baac141df191edc327
Eltano1985 <e.amoresano85@gmail.com>
2024-11-08 19:56:31 -0300

    documentado maphandler

--- MapHandler.js
+++ MapHandler.js
@@ -1,40 +1,58 @@
 class MapHandler {
     constructor(mapId) {
-        this.map = L.map(mapId).setView([20.5937, 78.9629], 5); // Coordenadas iniciales (India)
+        this.map = L.map(mapId).setView([20.5937, 78.9629], 5);
         this.marker = null;
-        this.activityMarkers = []; // Array para almacenar marcadores de actividades
-  
-        // Cargar las capas de OpenStreetMap
+        this.activityMarkers = [];
+
+        /**
+        * Adds an OpenStreetMap map layer to the map object
+        */
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
     }
-  
+
+    /**
+     * Adds a marker on the map at the provided latitude and longitude coordinates. If a marker already exists on the map, it deletes it before adding the new marker
+     * @param {number} lat 
+     * @param {number} lon 
+     * @param {string} destination 
+     */
     addMarker(lat, lon, destination) {
-        // Eliminar marcador anterior si existe
         if (this.marker) {
             this.marker.remove();
         }
-  
-        // Añadir nuevo marcador
         this.marker = L.marker([lat, lon]).addTo(this.map).bindPopup(`Destino: ${destination}`).openPopup();
     }
-  
+
+    /**
+     * Adds a marker on the map for a selected activity at the provided latitude and longitude coordinates. The marker includes a popup that shows the name of the activity and its index in the list
+     * @param {number} lat 
+     * @param {number} lon 
+     * @param {string} placeName 
+     * @param {number} index 
+     */
     addActivityMarker(lat, lon, placeName, index) {
-        // Añadir un marcador para actividades seleccionadas
         const activityMarker = L.marker([lat, lon]).addTo(this.map).bindPopup(`Actividad ${index}: ${placeName}`);
-        this.activityMarkers.push(activityMarker); // Almacenar el marcador en el array
+        this.activityMarkers.push(activityMarker);
+    }
+    
+    /**
+     * Removes all activity markers from the map and cleans the array that stores them
+     */
+    removeActivityMarkers() {
+        this.activityMarkers.forEach(marker => marker.remove());
+        this.activityMarkers = [];
     }
 
-    removeActivityMarkers() {
-        // Eliminar todos los marcadores de actividades
-        this.activityMarkers.forEach(marker => marker.remove());
-        this.activityMarkers = []; // Limpiar el array
-    }
-  
+    /**
+     * Sets the map view to a specific latitude and longitude coordinates with a zoom level fixed to 12
+     * @param {number} lat 
+     * @param {number} lon 
+     */
     setView(lat, lon) {
         this.map.setView([lat, lon], 12);
     }
 }
-  
+
 export default MapHandler;
